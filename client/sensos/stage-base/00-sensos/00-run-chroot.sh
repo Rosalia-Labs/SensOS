70c676cf0cdd7782be5481b779fcc0667a903257
Timothy Keitt <tkeitt@gmail.com>
2025-03-17 15:57:27 -0500

    added changing eeprom settings

--- client/sensos/stage-base/00-sensos/00-run-chroot.sh
+++ client/sensos/stage-base/00-sensos/00-run-chroot.sh
@@ -2,3 +2,6 @@
 
 # Update the CA certificates in the target system
 update-ca-certificates
+
+# Enable i2c
+raspi-config nonint do_i2c 0
--- client/sensos/stage-base/00-sensos/00-run.sh
+++ client/sensos/stage-base/00-sensos/00-run.sh
@@ -5,6 +5,8 @@ install -m 644 files/domain.crt "${ROOTFS_DIR}/usr/local/share/ca-certificates/d
 
 # Install the sensos client configuration script
 install -m 755 files/config-sensos-client "${ROOTFS_DIR}/usr/local/bin"
-
+install -m 755 files/config-eeprom "${ROOTFS_DIR}/usr/local/bin"
 install -m 755 files/monitor-connectivity.sh "${ROOTFS_DIR}/usr/local/bin"
 install -m 644 files/monitor-connectivity.service "${ROOTFS_DIR}/etc/systemd/system/monitor-connectivity.service"
+
+install -m 755 files/config-eeprom-once "${ROOTFS_DIR}/etc/init.d/"
