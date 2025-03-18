8677153788234931987f3860d3a2f41dffd00a8b
Timothy Keitt <tkeitt@gmail.com>
2025-03-18 16:07:14 -0500

    fixed for latest docker

--- client/sensos/stage-base/00-sensos/00-run-chroot.sh
+++ client/sensos/stage-base/00-sensos/00-run-chroot.sh
@@ -2,3 +2,27 @@
 
 # Enable i2c
 raspi-config nonint do_i2c 0
+
+apt-get update &&
+    apt-get install -y --no-install-recommends \
+        ca-certificates \
+        curl
+
+# Create directory for Docker's GPG key and add it
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add Docker repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \
+https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
+    >/etc/apt/sources.list.d/docker.list
+
+# Update package lists
+apt-get update &&
+    apt-get install -y --no-install-recommends \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io \
+        docker-buildx-plugin \
+        docker-compose-plugin
