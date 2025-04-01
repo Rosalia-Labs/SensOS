927865ec0855ad469568c5e4f2c1ba748a4fb704
Timothy H. Keitt <tkeitt@utexas.edu>
2025-04-01 11:28:08 -0500

    image build tweaks

--- client/sensos/stage-base/00-sensos/06-run.sh
+++ client/sensos/stage-base/00-sensos/06-run.sh
@@ -9,7 +9,7 @@ install -d -m 2755 -o root -g systemd-journal /var/log/journal
 grep -q 'i2c_bcm2835' /etc/modules || \
   echo 'i2c_bcm2835' >> /etc/modules
 
-# Avoid duplicate dtparam entries
+# Modem settings -- should be optional
 grep -q '^dtparam=i2c_arm_baudrate=' /boot/firmware/config.txt || \
   echo 'dtparam=i2c_arm_baudrate=10000' >> /boot/firmware/config.txt
 EOF
