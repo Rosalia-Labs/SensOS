07179bef6212999ed6a8593b82d8e25d86487bfd
Timothy H. Keitt <tkeitt@utexas.edu>
2025-04-27 18:05:56 -0500

    path fix

--- server/test/wireguard/run-test.sh
+++ server/test/wireguard/run-test.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Config
-MODULE_FILE="../../controller/wireguard.py"
+MODULE_FILE="../../docker/controller/wireguard.py"
 TEST_FILE="test.py"
 WORKDIR="$(mktemp -d)"
 
@@ -11,10 +11,10 @@ cp "$MODULE_FILE" "$TEST_FILE" "$WORKDIR/"
 
 # Run docker
 docker run --rm -it \
-    -v "$WORKDIR":/app \
-    -w /app \
-    debian:bookworm-slim \
-    bash -c "
+  -v "$WORKDIR":/app \
+  -w /app \
+  debian:bookworm-slim \
+  bash -c "
       apt-get update && \
       apt-get install -y python3 python3-pip python3-venv wireguard-tools && \
       python3 -m venv venv && \
