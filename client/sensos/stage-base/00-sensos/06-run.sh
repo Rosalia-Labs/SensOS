5d801d3450b860048f99c60db5cae4b1effb9722
Timothy Keitt <tkeitt@gmail.com>
2025-06-20 20:02:52 -0500

    send update systemd service

--- client/sensos/stage-base/00-sensos/06-run.sh
+++ client/sensos/stage-base/00-sensos/06-run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# Setup chrony as the NTP client and disable systemd-timesyncd
 
+# Setup chrony as the NTP client and disable systemd-timesyncd
 on_chroot <<EOF
 ln -sf /sensos/etc/chrony.conf /etc/chrony/chrony.conf
 systemctl enable chrony
 systemctl disable systemd-timesyncd
+EOF
+
+# Enable status updates
+on_chroot <<EOF
+systemctl enable send-status-update.timer
 EOF
